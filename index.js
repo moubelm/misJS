bcb8b65a5adae3b61b961e3a7460020fb2ef2e4b
moubelm <moubelm@gmail.com>
2022-08-04 12:35:18 +0200

    suviendo mi primer js

---
+++ index.js
@@ -0,0 +1,9 @@
+fetch('https://pokeapi.co/api/v2/pokemon') 
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data.results)
+        data.results.forEach(element => {
+            console.log(element)
+        });
+    })
+    .catch(error => console.log(error))
